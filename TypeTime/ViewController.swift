18af9dd9fa7c665341c0a49950e1f8476042a7a2
Rui-Wei Zhao <rw.du.zhao@gmail.com>
2015-05-01 20:22:42 +0800

    Initial Commit

---
+++ TypeTime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TypeTime
+//
+//  Created by Rui-Wei Zhao on 5/1/15.
+//  Copyright (c) 2015 Rui-Wei Zhao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ TypeTimeTests/TypeTimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  TypeTimeTests.swift
+//  TypeTimeTests
+//
+//  Created by Rui-Wei Zhao on 5/1/15.
+//  Copyright (c) 2015 Rui-Wei Zhao. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class TypeTimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

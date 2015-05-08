f98a88bf735493b20ddef63257e86b8fbb17238a
Rui-Wei Zhao <rw.du.zhao@gmail.com>
2015-05-09 01:35:46 +0800

    nice up code

--- TypeTime/MainWindowController.swift
+++ TypeTime/MainWindowController.swift
@@ -12,8 +12,6 @@ class MainWindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
-
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
     
 }
